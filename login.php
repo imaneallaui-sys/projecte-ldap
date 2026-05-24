3869636b66beef98b31b1615ca0cae73e35af731
Imane <imane.allaui@cirvianum.cat>
2026-05-24 17:08:35 +0000

    actualización del proyecto

--- login.php
+++ login.php
@@ -17,29 +17,28 @@ $pass = $_POST['password'];
 // Conectar con el servidor LDAP
 $conn = ldap_connect($ldap_host);
 
-// Usar protocolo LDAP v3 
+// Usar protocolo LDAP v3 (obligatorio)
 ldap_set_option($conn, LDAP_OPT_PROTOCOL_VERSION, 3);
 
 // Comprobar conexión
 if ($conn) {
 
     // Intento de autenticación (bind LDAP)
-    // OJO: buscamos el usuario dentro de ou=grup1
-    $bind = @ldap_bind($conn, "uid=$user,ou=grup1,$ldap_dn", $pass);
+    // Busca el usuario en TODO el LDAP (grup1 + grup2)
+    $bind = @ldap_bind($conn, "uid=$user,$ldap_dn", $pass);
 
     // Si la autenticación es correcta
     if ($bind) {
         header("Location: success.php");
         exit();
     } 
-    // Si falla la autenticación
+    // Si la autenticación falla
     else {
         header("Location: error.html");
         exit();
     }
 
 } else {
-    // Si no se puede conectar al LDAP
     echo "Error de conexión con el servidor LDAP";
 }
 
